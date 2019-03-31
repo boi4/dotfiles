65ee48769d9940a0e391be2f32aeec413a98f54f
Jan Fecht <jan.fecht@tum.de>
2019-04-01 01:20:39 +0200

    ipython changes

--- .config/python/startup.py
+++
@@ -1,6 +0,0 @@
-from math import *
-
-def binom(a,b):
-    if b>a or a < 0 or b < 0:
-        raise ValueError("Bad input for binomial function")
-    return factorial(a)/(factorial(b)*factorial(a-b))
